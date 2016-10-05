a0540fd18945b13d13e9494d44edca84e77f50c7
andreumas <andreumasandres1995@gmail.com>
2016-10-05 12:58:40 +0200

    Add files via upload

---
+++ pInvadersF1-master/lib/ia.php
@@ -0,0 +1,23 @@
+<?php
+class ia
+{
+
+public $fila = "2";
+ public $columna = "3";
+
+//declaracion getcolumna i getfila
+public function getColumna(){
+  return $this->fila;
+}
+public function getFila(){
+ return $this->columna;
+}
+
+
+
+}
+
+
+
+
+?>
