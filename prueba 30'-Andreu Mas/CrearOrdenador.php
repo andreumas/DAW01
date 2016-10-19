99064e314442aacbaf792da9dab25eb8810ee667
andreumas <andreumasandres1995@gmail.com>
2016-10-19 12:27:07 +0200

    Add files via upload

---
+++ prueba 30'-Andreu Mas/CrearOrdenador.php
@@ -0,0 +1,15 @@
+
+<?php  
+ 'Ordenador.php' {
+
+echo $ordenador1=$marca2 & $ram2;
+echo $ordenador2=$marca3 & $ram3;
+
+
+
+}
+
+
+
+
+?>
---
+++ prueba 30'-Andreu Mas/Ordenador.php
@@ -0,0 +1,29 @@
+<?php  
+
+class ordenador {
+
+    //variables marcas
+
+        
+        $marca1= "hp";
+        $marca2= "apple";
+        $marca3= "Asus";
+
+
+	//variables ram
+
+         $ram1=4;
+         $ram2=8;
+         $ram3=16;
+
+	}
+
+
+
+
+
+}
+
+
+
+?>
