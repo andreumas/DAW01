da16d132ab5424418c0cec1318da9204931b18dd
andreumas <andreumasandres1995@gmail.com>
2016-11-10 09:28:36 +0100

    Add files via upload

--- pInvadersF2-master/lib/ia.php
+++ pInvadersF2-master/lib/ia.php
@@ -2,17 +2,30 @@
 Class ia{
 var $columna;
 var $fila;
-	
+
 public function getColumna()
     {
 		$columna=rand(1,3);
         return $columna;
-		
+
     }
 	public function getFila()
     {
 		$fila=rand(1,4);
         return $fila ;
     }
+
+
+		public function setColumna($col)
+		    {
+				$columna=rand(1,3);
+		         $this->columna=$col;
+
+		    }
+			public function setFila($fil)
+		    {
+				$fila=rand(1,4);
+		        $this->fila=$fil ;
+		    }
 }
 ?>
